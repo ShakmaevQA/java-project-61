3b20a5c2e07441290b848fea674240d54f490547
ShakmaevT <timur@timur.>
2024-12-01 03:42:06 +0300

    Задание 3-2

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,11 +3,14 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    public static void inputName() {
+    public static void userName() {
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Welcome to the Brain Games!");
-        System.out.print("May I have your name? ");
-        String name = scanner.nextLine();
+        System.out.print("May I have your name?");
+        String name = scanner.next();
         System.out.println("Hello, " + name + "!");
+
+        scanner.close();
     }
 }
